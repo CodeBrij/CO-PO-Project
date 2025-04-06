4910042ce69dd2d691d9e0a55f414717296be3ee
BKCODE2003 <bhushankor12345.9@gmail.com>
2025-04-06 20:38:17 +0530

    CO Screen Fix to open At center

--- co.py
+++ co.py
@@ -32,13 +32,16 @@ class User_mode:
             screen_width=co_window.winfo_screenwidth()
             screen_height=co_window.winfo_screenheight()
        
-        # Calculate the coordinates for centering the window
-            x_position = 0
-            y_position = 0
+            # Set window size (like 80% of screen)
+            window_width = int(screen_width * 0.8)
+            window_height = int(screen_height * 0.8)
+            # Center the window
+            x = (screen_width - window_width) // 2
+            y = (screen_height - window_height) // 2
 
-             # Create a new CO Calculations window
+            # Create a new CO Calculations window
             co_window.title("CO Calculations")
-            co_window.geometry(f"{screen_width}x{screen_height}+{x_position}+{y_position}")
+            co_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
             main_frame = ctk.CTkFrame(master=co_window)
             main_frame.pack(expand=True, fill="both", padx=10, pady=10)
